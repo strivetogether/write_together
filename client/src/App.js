75a1b9381f762f3bdf4bbbf1233edd47e7592c20
Jan <jan.leon.voelker@gmail.com>
2020-12-15 17:56:49 +0100

    Added Ale's AddBlock.js manually after merge somehow overwrote it

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import AddBlock from './components/AddBlock';
 import Login from './components/Login';
 import Explore from './components/Explore';
 import BlockDetails from './components/BlockDetails';
-import { Editor } from '@tinymce/tinymce-react';
+import EditBlock from './components/EditBlock'
 
 
 
@@ -67,25 +67,6 @@ class App extends React.Component {
       <div className="App">
       
         <Navbar user={this.state.user} setUser={this.setUser}/>
-{/* 
-        <Editor
-        apiKey={process.env.REACT_APP_TINY_ID}
-         initialValue="<p>This is the initial content of the editor</p>"
-         init={{
-           height: 500,
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link image charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'insertdatetime media table paste code help wordcount'
-           ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | \
-             alignleft aligncenter alignright alignjustify | \
-             bullist numlist outdent indent | removeformat | help'
-         }}
-         onEditorChange={this.handleEditorChange}
-       /> */}
 
         <Route
         exact
@@ -132,6 +113,12 @@ class App extends React.Component {
           exact
           path='/blocks/:id'
           render={props => <BlockDetails setUser={this.setUser} blocks={this.state.blocks} ideas={this.state.ideas} getData={this.getData} {...props} />}
+        />
+
+<Route
+          exact
+          path='/blocks/:id/editblock'
+          render={props => <EditBlock setUser={this.setUser} blocks={this.state.blocks} ideas={this.state.ideas} getData={this.getData} {...props} />}
         />
 
         {/* <AddBlock /> */}
--- client/src/components/EditBlock.js
+++ client/src/components/EditBlock.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'react-bootstrap';
 import axios from 'axios';
+import { Markup } from 'interweave';
+import Interweave from 'interweave';
+import { Form, Button } from 'react-bootstrap';
 import { Editor } from '@tinymce/tinymce-react';
 
 
-console.log('HERE WE ARE')
-
-export default class AddBlock extends Component {
-
+export default class EditBlock extends Component {
   state = {
     title: '',
     text: '',
@@ -59,7 +58,7 @@ export default class AddBlock extends Component {
     }
 
   render() {
-
+    console.log('HERE WE ARE 2')
     return (
       
         <Form onSubmit={this.handleSubmit}>
@@ -70,11 +69,15 @@ export default class AddBlock extends Component {
             type='text'
             id='title'
             name='title'
-            value={this.state.title}
+            value={this.props.title}
             onChange={this.handleChange}
           />
         </Form.Group>
         
+
+
+       
+
         <Form.Group>
           <Form.Label htmlFor='text'>Your text so far: </Form.Label>
           <Editor
@@ -84,25 +87,73 @@ export default class AddBlock extends Component {
           value={this.state.text}
           id="text"
           initialValue="<p>This is the initial content of the editor</p>"
-          init={{
-            min_height: 300,
-            // height: '50vh',
-           fontsize_formats: "8pt 10pt 12pt 14pt 18pt 24pt 36pt",
-           menubar: false,
-           plugins: [
-             'advlist autolink lists link charmap print preview anchor',
-             'searchreplace visualblocks code fullscreen',
-             'paste code help wordcount'
-           ],
-           toolbar:
-             'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat |  fontsizeselect | help'
+              init={{
+                skin: "fabric",
+                icon: "jam",
+                branding: false,
+                plugins:
+                  "print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons autoresize",
+                menubar: "file edit view insert format tools table help",
+                toolbar_mode: "floating",
+                toolbar: [
+                  {
+                    name: "history",
+                    items: ["undo", "redo"],
+                  },
+                  {
+                    name: "formatting",
+                    items: ["bold", "italic", "underline", "strikethrough"],
+                  },
+                  {
+                    name: "alignment",
+                    items: [
+                      "alignleft",
+                      "aligncenter",
+                      "alignright",
+                      "alignjustify",
+                    ],
+                  }, {
+                    name: "preview",
+                    items:["fullscreen", "preview" ,"save", "print" ],
+                  },
+                  {
+                    name: "indentation",
+                    items: ["outdent", "indent"],
+                  }, {
+                    name: "list",
+                    items:["numlist" , "bullist"],
+                  }, {
+                    name: "color",
+                    items:["forecolor", "backcolor", "removeformat" ],
+                  }, {
+                    name: "emoticons",
+                    items:["charmap", "emoticons" ],
+                  }
+                ],
+                toolbar_sticky: true,
+                image_advtab: true,
+                template_cdate_format:
+                  "[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]",
+                template_mdate_format:
+                  "[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]",
+                min_height: 600,
+                width: 600,
+                min_width: 600,
+                resize: false,
+                statusbar: false,
+                image_caption: true,
+                quickbars_selection_toolbar:
+                  "bold italic | quicklink h2 h3 blockquote quickimage quicktable",
+                noneditable_noneditable_class: "mceNonEditable",
+                contextmenu: "link image imagetools table",
          }}
          onEditorChange={this.handleTextChange}
         />
         </Form.Group>
 
+
         <Form.Group>
-          <Form.Label htmlFor='text'>Your text so far: </Form.Label>
+          <Form.Label htmlFor='text'>Your questions or block to solve: </Form.Label>
           <Editor
           apiKey={process.env.REACT_APP_TINY_ID}
           type="text"
